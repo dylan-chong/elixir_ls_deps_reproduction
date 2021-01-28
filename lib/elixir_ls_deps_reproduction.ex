cc4c487ab8137942b12696facda556d9afba892a
Dylan Chong <dylan.chong@icloud.com>
2021-01-28 13:16:31 +1300

    Initial commit

---
+++ lib/elixir_ls_deps_reproduction.ex
@@ -0,0 +1,18 @@
+defmodule ElixirLsDepsReproduction do
+  @moduledoc """
+  Documentation for `ElixirLsDepsReproduction`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirLsDepsReproduction.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_ls_deps_reproduction_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirLsDepsReproductionTest do
+  use ExUnit.Case
+  doctest ElixirLsDepsReproduction
+
+  test "greets the world" do
+    assert ElixirLsDepsReproduction.hello() == :world
+  end
+end
